4529dfe39d270c9ba69b55954919db7d03014e76
Sjupj <sjupjj@gmail.com>
2022-02-04 00:55:14 +0300

    12345

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов просмотрели? ', '');
+function showFirstMessage(text) {
+  console.log(text);
+}
 
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+showFirstMessage('Hellow!');
 
-let movie = prompt('Один из последних просмотренных фильмов? ', '');
-let raiting = +prompt('На сколько вы его оцениете? ', '');
 
-personalMovieDB.movies[movie] = raiting;
+// let numberOfFilms = +prompt('Сколько фильмов просмотрели? ', '');
 
-console.log(personalMovieDB);
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//   const movie = prompt('Один из последних просмотренных фильмов? ', '');
+//   const raiting = +prompt('На сколько вы его оцениете? ', '');
+
+//   if (movie != null && raiting != null && movie != '' && raiting != '' && movie.length < 50) {
+//     personalMovieDB.movies[movie] = raiting;
+//     console.log('Done!');
+//   } else {
+//     console.log('Error!');
+//     i--;
+//   }
+// }
+
+// console.log(personalMovieDB);
