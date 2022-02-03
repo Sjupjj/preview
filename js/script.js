efbed2ee65e737740eccc3e3ff2c9ad0d5eb30e3
Sjupj <sjupjj@gmail.com>
2022-02-03 11:32:04 +0300

    Первая практика

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 "use strict";
 
-const userName = 'Stas';
-console.log(userName);
+let numberOfFilms = +prompt('Сколько фильмов просмотрели? ', '');
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+let movie = prompt('Один из последних просмотренных фильмов? ', '');
+let raiting = +prompt('На сколько вы его оцениете? ', '');
+
+personalMovieDB.movies[movie] = raiting;
+
+console.log(personalMovieDB);
